1a45abe8b1337a2bd5ba466a07c18205ac3fb964
Marc <marcalfaro@live.com>
2020-03-15 23:01:54 +0800

    Add project files.

---
+++ FlashTaskbar/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    <DllImport("user32.dll", EntryPoint:="FlashWindow")>
+    Public Shared Function FlashWindow(ByVal hwnd As Integer, ByVal bInvert As Integer) As Integer
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FlashWindow(Me.Handle, 1)
+    End Sub
+
+End Class
